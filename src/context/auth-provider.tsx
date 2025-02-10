497528465dde4d4fce1f0392994b1f5a713e50fe
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-02-10 18:19:10 +0800

    create workspace functionality

--- src/context/auth-provider.tsx
+++ src/context/auth-provider.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext, useContext, useEffect } from "react";
-//import useWorkspaceId from "@/hooks/use-workspace-id";
+import useWorkspaceId from "@/hooks/use-workspace-id";
 import useAuth from "@/hooks/api/use-auth";
-import { UserType } from "@/types/api.type";
+import { UserType, WorkspaceType } from "@/types/api.type";
+import { getWorkspaceByIdQueryFn } from "@/lib/api";
+import useGetWorkspaceQuery from "@/hooks/api/use-get-workspace";
+import { useNavigate } from "react-router-dom";
 
 // Define the context shape
 type AuthContextType = {
-  user?:UserType
+  user?:UserType;
+  workspace?:WorkspaceType;
   error:any;
   isLoading: boolean;
+  workspaceLoading:boolean;
   isFetching:boolean;
   refetchAuth:()=>void;
+  refetchWorkspace:()=>void;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -18,30 +24,48 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  //const navigate = useNavigate();
+  const navigate=useNavigate();
+  const workspaceId = useWorkspaceId();
 
   const {
     data:authData, 
     error:authError,
-    isLoading:authLoading,
+    isLoading,
     isFetching,
     refetch:refetchAuth
   }=useAuth();
 
+  const {
+    data:workspaceData, 
+    isLoading:workspaceLoading,
+    error:workspaceError,
+    refetch:refetchWorkspace
+  }=useGetWorkspaceQuery(workspaceId);
+
+  const workspace=workspaceData?.workspace;
+
 
   const user=authData?.user;
-  //const workspaceId = useWorkspaceId();
 
-  useEffect(() => {});
+  useEffect(() => {
+    if(workspaceError){
+      if(workspaceError?.errorCode==='ACCESS_UNAUTHORIZED'){
+        navigate("/")
+      }
+    }
+  },[navigate, workspaceError]);
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        error:authError,
-        isLoading:authLoading,
+        workspace,
+        error:authError || workspaceError,
+        isLoading,
+        workspaceLoading,
         isFetching,
-        refetchAuth
+        refetchAuth, 
+        refetchWorkspace
       }}
     >
       {children}
